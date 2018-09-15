54881773cdb9a18a2ab8779b106edf95a5191d02
MeerKat_devs <30046653@student.g.nwu.ac.za>
2018-09-15 17:05:46 +0200

    Fix Morse.cpp
    Morse.cpp displays entered word in morse code

--- Morse.cpp
+++ Morse.cpp
@@ -7,31 +7,23 @@ using namespace std;
 
 main () {
 //Variables
-    char word[10], outMorse[100], alphabet[26][1], morse[26][9];
+    char word[10], outMorse[10][10], alphabet[26][2], morse[26][9];
     int cnt = 0;
 //#################################
 
     ifstream alphaFile, morseFile;
     morseFile.open("Morse.txt");
     alphaFile.open("Alphabet.txt");
-    if (!alphaFile) {
-        cout << "Clould not open alphabet file" << endl;
-        exit(1);
-    }
-    if (!morseFile) {
-        cout << "Clould not open Morse code file" << endl;
-        exit(1);
-    }
     while (alphaFile) {
-        alphaFile.getline(alphabet[cnt],1);
-        morseFile.getline(morse[cnt],9);
+        alphaFile.getline(alphabet[cnt],2);
+        morseFile.getline(morse[cnt],9);    
         cnt++;
     }
     cout << "Enter word to convert to morse code: ";
     cin >> word;
     for (int a = 0; a < strlen(word); a++) {
         for (int b = 0; b < 26; b++) {
-            if (strcmp(word[a],alphabet[b]) == 0) {
+            if (word[0] == alphabet[b][0]) {
                 strcpy(outMorse[a],morse[b]);
             }
         }
